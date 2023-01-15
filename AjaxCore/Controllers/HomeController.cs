2b00c900e01af48d0746824d6da588220691e123
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-01-15 17:11:26 +0300

    Mini Project anded

--- AjaxCore/Controllers/HomeController.cs
+++ AjaxCore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -38,5 +39,27 @@ namespace AjaxCore.Controllers
             var Kullanici = context.Kullanicilar.FirstOrDefault( I => I.ID==KullaniciId);
             return Json(JsonConvert.SerializeObject(Kullanici));
         }
+
+        [HttpPost]
+        public void Guncelle(Kullanici kullanici)
+        {
+            using var context = new Context();
+            var guncellenenKullanici = context.Kullanicilar.Find(kullanici.ID);
+
+            if (kullanici.File != null)
+            {
+                var benzersizIsim = Guid.NewGuid() + Path.GetExtension(kullanici.File.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + benzersizIsim);
+                var stream = new FileStream(path,FileMode.Create);
+                kullanici.File.CopyTo(stream);
+
+                guncellenenKullanici.ResimAd = benzersizIsim;
+            }
+
+            guncellenenKullanici.AdSoyad = kullanici.AdSoyad;
+            context.Kullanicilar.Update(guncellenenKullanici);
+            context.SaveChanges();
+        }
+
     }
 }
--- AjaxCore/Models/Kullanici.cs
+++ AjaxCore/Models/Kullanici.cs
@@ -1,9 +1,16 @@
-﻿namespace AjaxCore.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace AjaxCore.Models
 {
     public class Kullanici
     {
         public int ID { get; set; }
         public string AdSoyad { get; set; }
         public string ResimAd { get; set; }
+
+        [NotMapped] 
+        public IFormFile File { get; set; }
     }
 }
